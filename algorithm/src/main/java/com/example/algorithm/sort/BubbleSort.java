123544b2b86ece8b9813d050250558faff548dc8
comdd <dongld.li@qunar.com>
2019-01-13 13:08:51 +0800

    冒泡排序

---
+++ algorithm/src/main/java/com/example/algorithm/sort/BubbleSort.java
@@ -0,0 +1,30 @@
+package com.example.algorithm.sort;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Description :
+ *
+ * @author : dd
+ */
+public class BubbleSort implements Sort {
+    @Override
+    public List<Integer> sort(Integer[] source) {
+        for (int i = 0; i < source.length - 1; i++) {
+            for (int j = 0; j < source.length - i-1; j++) {
+                if (source[j] > source[j + 1]) {
+                    swap(source, j, j + 1);
+                }
+            }
+        }
+        return Arrays.asList(source);
+    }
+
+    private void swap(Integer[] source, Integer left, Integer right) {
+        Integer tmp = source[left];
+        source[left] = source[right];
+        source[right] = tmp;
+
+    }
+}
